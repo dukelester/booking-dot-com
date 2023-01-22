a82e707f14b3e8875b48d8b1aea6b72f777213c8
dukelester <dukelester4@gmail.com>
2023-01-22 11:46:27 +0300

    Added the database connection to MongoDb
    Using the mongoose library, I configured the connections to the database

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
-
 dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('The database has been connected successfully');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongo db disconnected!');
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo db disconnected!');
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`server running at http://127.0.0.1:${port}`);
+  connect();
+  console.log(`server running at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('<h2> Welcome to our booking application</h2>');
 });
