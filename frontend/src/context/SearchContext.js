1c707a35bad49972d1edd5ed7a4bd3c80db4012b
dukelester <dukelester4@gmail.com>
2023-01-25 02:15:18 +0300

    added the Context provider to the entire application.
    This will pass data from one component to the other down the application

--- frontend/src/context/SearchContext.js
+++ frontend/src/context/SearchContext.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { createContext } from "react";
 
 const INITIAL_STATE = {
@@ -11,3 +12,24 @@ const INITIAL_STATE = {
 };
 
 export const SearchContext  = createContext(INITIAL_STATE);
+
+const SearchReducer = (state, action) => {
+  switch(action.type) {
+    case "NEW_SEARCH":
+        return action.payload;
+    case 'RESET_SEARCH':
+        return INITIAL_STATE;
+    default:
+        return state;
+  }
+};
+
+export const SearchContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE);
+    return(
+        <SearchContext.Provider
+        value={ { city: state.city, dates: state.dates, options: state.options, dispatch } } >
+            { children }
+        </SearchContext.Provider>
+    )
+}
