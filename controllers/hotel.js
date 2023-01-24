e9797f97bea5e4d591725a6daef46a38d64143dc
dukelester <dukelester4@gmail.com>
2023-01-24 11:21:15 +0300

    Added the count by type
    Here we can count hotesl, appartments, cabins, villas or resorts.

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -26,15 +26,6 @@ export const getHotelById = async (req, res, next) => {
   }
 };
 
-export const getAllHotels = async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (error) {
-    next(error);
-  }
-};
-
 export const updateHotelById = async (req, res, next) => {
   try {
     const { hotelId } = req.params;
@@ -50,6 +41,44 @@ export const deleteHotel = async (req, res, next) => {
     const { hotelId } = req.params;
     await Hotel.findByIdAndDelete(hotelId);
     res.status(200).json({ message: `Hotel with id ${hotelId} has been deleted successfully` });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(',');
+  try {
+    const list = await Promise.all(cities.map((city) => Hotel.countDocuments({ city })));
+    res.status(200).json(list);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: 'hotel' });
+    const apartmentCount = await Hotel.countDocuments({ type: 'apartment' });
+    const resortCount = await Hotel.countDocuments({ type: 'resort' });
+    const villaCount = await Hotel.countDocuments({ type: 'villa' });
+    const cabinCount = await Hotel.countDocuments({ type: 'cabin' });
+    res.status(200).json([
+      { type: 'hotel', count: hotelCount },
+      { type: 'apartment', count: apartmentCount },
+      { type: 'resort', count: resortCount },
+      { type: 'villa', count: villaCount },
+      { type: 'cabin', count: cabinCount },
+    ]);
   } catch (error) {
     next(error);
   }
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,17 +1,12 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import {
+  countByCity, countByType,
   createHotel, deleteHotel, getAllHotels, getHotelById, updateHotelById,
 } from '../controllers/hotel.js';
 import { verifyIsAdmin } from '../utils/verifytoken.js';
 
 const router = express.Router();
-
-// GET ALL
-router.get('/', getAllHotels);
-
-// GET Hotel by ID
-router.get('/:hotelId', getHotelById);
 
 // CREATE
 router.post('/', verifyIsAdmin, createHotel);
@@ -21,5 +16,12 @@ router.put('/:hotelId', verifyIsAdmin, updateHotelById);
 
 // DELETE
 router.delete('/:hotelId', verifyIsAdmin, deleteHotel);
+// GET Hotel by ID
+router.get('/find/:hotelId', getHotelById);
+
+// GET ALL
+router.get('/', getAllHotels);
+router.get('/countByCity', countByCity);
+router.get('/countByType', countByType);
 
 export default router;
