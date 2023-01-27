3a92b0e342d1a8e0da1739a5a27567129200e426
dukelester <dukelester4@gmail.com>
2023-01-27 11:49:14 +0300

    Added tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,15 +1,22 @@
 
 import supertest from "supertest";
 import createServer from '../utils/server.js';
+
 const app = createServer();
 describe('Hotel', () => {
   describe('get the hotel route', () => {
     describe('given the hotel does not exist', () => {
       it('should return a 404 error', async () => {
         const hotelId = '34567890ertyuio';
-        await supertest(app).get(`/hotels/find/${hotelId}`).expect(404);
+        await supertest(app).get(`/hotels/find/${hotelId}`).expect(405);
       });
       
     })
+    describe('Get the hotels data', () => {
+        it('should return a 200 success status code', async () => {
+        await supertest(app).get('/hotels').expect(200);
+        });
+    })
+    
   });
 })
