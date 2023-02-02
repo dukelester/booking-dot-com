6315f477cdddd5f0e93b9a68060104c9a3bcb7d3
dukelester <dukelester4@gmail.com>
2023-02-02 10:28:12 +0300

    Implemented the confirmation email sending

---
+++ utils/send-email.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+import { createTransport } from 'nodemailer';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const sendUser = process.env.EMAIL_HOST_USER;
+const userPass = process.env.EMAIL_HOST_PASSWORD;
+
+const transport = createTransport({
+  service: 'Gmail',
+  auth: {
+    user: sendUser,
+    pass: userPass,
+  },
+});
+
+const sendConfirmationEmail = (name, email, confirmationCode) => {
+  console.log('check ...');
+  transport.sendMail({
+    from: sendUser,
+    to: email,
+    subject: 'Please confirm your email',
+    html: `<h1>Email Confirmation</h1>
+    <h2>Hello ${name}</h2>
+    <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+    <a href=http://localhost:5000/confirm/${confirmationCode}> Click here</a>
+    </div>`,
+  }).catch((error) => {
+    console.log(error);
+  });
+};
+
+export default sendConfirmationEmail;
